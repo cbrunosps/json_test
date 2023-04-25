e90565c7306b398a44104c139310ace62da5d6af
JosueCruzBarragan <jcruz@spsolutions.com.mx>
2023-04-25 17:56:48 -0600

    correccion long caract

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
             if dic['Resources'][item]['Type'] == 'AWS::Serverless::Function':
                 try:
                     name = dic['Resources'][item]['Properties']['FunctionName']['Fn::Sub']
-                    if len(name) <= 100:
+                    if len(name) <= 10:
                         print(f'-> Para "{item}" el nombre: "{name}", esta en la longitud correcta. [✅]')
                     else:
                         print(f'-> Para "{item}" el nombre: "{name}", es demaciado largo. [❌]')
@@ -21,7 +21,7 @@ def main():
                 # Si en recurso NO existe "FunctionName", lo ignoramos
                 except:
                     print(f'No existe "Properties":"FunctionName" en {item} \n')
-            if const != 0: exit(123)
+            if const != 0: exit("Error en la longitud de los caracteres permitidos.")
 
 
 if __name__ == '__main__':
